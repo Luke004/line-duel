e6182b5dc77e04737e52eadba0c05904c4253b4c
Lukas <l.hilfrich@gmx.de>
2021-05-06 00:08:03 +0200

    all players can intersect with all trails

--- main.js
+++ main.js
@@ -9,10 +9,31 @@ var gameOver = false
 
 function update(progress) {
     if (gameOver) return
+
+    let allTrailRects = []
+
     players.forEach((player) => {
         player.update(progress)
         handleBorderCollisions(canvas, player)
-    });
+
+        let trailRects = player.getTrailRects()
+        allTrailRects = allTrailRects.concat(trailRects)
+    })
+
+    // check the losing condition
+    players.forEach((player) => {
+        let intersection = false
+        for (let i = 0; i < allTrailRects.length; ++i) {
+            if (intersectRect(player.getPlayerRect(), allTrailRects[i])) {
+                intersection = true
+                break
+            }
+        }
+        if (intersection) {
+            alert("GAME OVER: Player '" + player.name + "' has lost!")
+            gameOver = true
+        }
+    })
 
 }
 
@@ -21,7 +42,7 @@ function draw() {
     //ctx.clearRect(0, 0, canvasWidth, canvasHeight)
     players.forEach((player) => {
         player.draw(canvas.ctx)
-    });
+    })
 }
 
 function loop(timestamp) {
--- player.js
+++ player.js
@@ -16,14 +16,14 @@ class Player {
         this.keyMap = Player.keyMap
         Player.keyMap += 1
 
-        // keeping track of the previous player movement
-        this.previous_movement_rects = []
-        this.previous_movement_rects_curr_idx = 0
-        this.currentRect = {
+        // keeping track of the player's trail rectangles
+        this.trail_rects = []
+        this.trail_rects_curr_idx = 0
+        this.current_trail_rect = {
             startX: this.x,
             startY: this.y
         }
-        this.next_rect = {
+        this.next_trail_rect = {
             left: -10,
             right: -10,
             top: -10,
@@ -59,22 +59,11 @@ class Player {
             left: this.x - this.currentSize / 2,
             right: this.x + this.currentSize / 2
         }
-
-        let intersection = false
-        for (let i = 0; i < this.previous_movement_rects.length; ++i) {
-            if (intersectRect(this.player_rect, this.previous_movement_rects[i])) {
-                intersection = true
-                break
-            }
-        }
-        if (intersection) {
-            alert("GAME OVER: Player '" + this.name + "' has lost!")
-            gameOver = true
-        }
+        
     }
 
     draw(ctx) {
-        ctx.fillStyle = this.color;
+        ctx.fillStyle = this.color
         ctx.fillRect(this.x - this.currentSize / 2, this.y - this.currentSize / 2, this.currentSize, this.currentSize)
     }
 
@@ -92,37 +81,45 @@ class Player {
         // each 30 units update the rectangle trail
         if (this.currentDistanceTraveled < 30) return
         this.currentDistanceTraveled = 0
-        // each time the player changes its direction, increment 'previous_movement_rects_curr_idx'
-        // in order to write to the next idx in the 'previous_movement_rects' array
+        // each time the player changes its direction, increment 'trail_rects_curr_idx'
+        // in order to write to the next idx in the 'trail_rects' array
         // if the player did not change the direction since last call, we simply write to the last idx
         if (this.hasChangedDirection) {
             this.hasChangedDirection = false
-            this.previous_movement_rects_curr_idx++
+            this.trail_rects_curr_idx++
         }
 
         // add the rect previously created from last function call (with a delay of 'currentDistanceTraveled')
         // this prevents the player intersecting with its own rectangle trail behind him because it is created too fast
-        this.previous_movement_rects[this.previous_movement_rects_curr_idx] = this.next_rect
+        this.trail_rects[this.trail_rects_curr_idx] = this.next_trail_rect
 
         // create the rect of the current players location for the next method iteration
         let rect = {
-            left: Math.min(this.currentRect.startX, this.x) - this.currentSize / 2,
-            right: Math.max(this.currentRect.startX, this.x) + this.currentSize / 2,
-            top: Math.min(this.currentRect.startY, this.y) - this.currentSize / 2,
-            bottom: Math.max(this.currentRect.startY, this.y) + this.currentSize / 2
+            left: Math.min(this.current_trail_rect.startX, this.x) - this.currentSize / 2,
+            right: Math.max(this.current_trail_rect.startX, this.x) + this.currentSize / 2,
+            top: Math.min(this.current_trail_rect.startY, this.y) - this.currentSize / 2,
+            bottom: Math.max(this.current_trail_rect.startY, this.y) + this.currentSize / 2
         }
         // persist it
-        this.next_rect = rect
+        this.next_trail_rect = rect
     }
 
     onDirectionChange() {
         // set starting points for the next trail rect
-        this.currentRect = {
+        this.current_trail_rect = {
             startX: this.x,
             startY: this.y
         }
         // notify 'buildWayRectangles' that the player has changed its direction
         this.hasChangedDirection = true
+    }
+
+    getTrailRects() {
+        return this.trail_rects
+    }
+
+    getPlayerRect() {
+        return this.player_rect
     }
 
 }
